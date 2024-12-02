43fb92462564860ea22bd1dd7b043595a35e71de
George Patterson <georgelpatterson2002@gmail.com>
2024-12-01 19:21:52 -0800

    Initial Commit With Data and Imports

---
+++ Rainfall_Prediction_Model.py
@@ -0,0 +1,12 @@
+# Data Handling
+import pandas as pd
+import numpy as np 
+
+# Machine Learning Tools
+from sklearn.model_selection import train_test_split, StratifiedKFold
+from sklearn.metrics import accuracy_score, roc_auc_score
+import lightgbm as lgb
+
+# Hyperparameter Tuning
+import optuna
+
